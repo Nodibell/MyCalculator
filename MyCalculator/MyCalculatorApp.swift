d9aebdbce6403d4e522e26afb3129a6cc5d52ca8
Oleksii <oleksiychumak2004@gmail.com>
2024-07-01 23:05:11 +0300

    Initial Commit

---
+++ MyCalculator/MyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculatorApp.swift
+//  MyCalculator
+//
+//  Created by Oleksii Chumak on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
